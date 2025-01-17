d964a4107a5605d24351350fec9a58c18c200f4d
codewithkael <76805421+codewithkael@users.noreply.github.com>
2025-01-17 23:54:18 +0330

    changing app icon

--- app/src/main/java/com/codewithkael/twitchwithwebrtc/utils/Constants.kt
+++ app/src/main/java/com/codewithkael/twitchwithwebrtc/utils/Constants.kt
@@ -6,6 +6,8 @@ object Constants {
     const val MAIN_SCREEN = "MainScreen"
     const val STREAMER_SCREEN = "StreamerScreen"
     fun getViewerScreen(streamId: String) = "ViewerScreen/$streamId"
-    val REMOTE_SOCKET_URL = "ws://164.92.142.251:3333/app/$STREAM_ID?direction=send"
-    fun getStreamPath(id:String)="ws://164.92.142.251:3333/app/$id"
+
+    private const val BASE_URL = "128.140.38.246"
+    val REMOTE_SOCKET_URL = "ws://$BASE_URL:3333/app/$STREAM_ID?direction=send"
+    fun getStreamPath(id:String)="ws://$BASE_URL:3333/app/$id"
 }
